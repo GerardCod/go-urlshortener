158caf7135437b2f44ee5e1cd4558811cb13687b
Gera <gerardo.aguilar.calderon@gmail.com>
2022-05-31 22:07:11 -0500

    modify: creating the first version of the executable

--- app/main.go
+++ app/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	urlshort "github.com/GerardCod/go-urlshortener"
+)
+
+func main() {
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	fmt.Println("Starting the server at port :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world")
 }
